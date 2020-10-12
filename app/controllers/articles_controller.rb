36a80db89e1f985ba026f2d3a189f6f4a78c5d3c
Bradley <bmcguigandev@outlook.com>
2020-10-12 09:04:34 +0100

    initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,45 @@
+class ArticlesController < ApplicationController
+    include ArticlesHelper
+
+    def index
+        @articles = Article.all
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            flash[:success] = "Article '#{@article.title}' successfully created."
+            redirect_to @article
+        else
+            flash.now[:error] = "Error: Article not created."
+            render(:new)
+        end
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+
+        redirect_to @article
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        flash[:success] = "Article '#{@article.title}' successfully deleted."
+        @article.destroy
+        
+        redirect_to articles_path
+    end
+end
