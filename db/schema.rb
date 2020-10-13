17527b6e8ffabd92ea2fb990c3f432e3cf0fe1fb
Bradley <bmcguigandev@outlook.com>
2020-10-13 03:55:00 +0100

    add basic authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_11_211704) do
+ActiveRecord::Schema.define(version: 2020_10_13_021338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 2020_10_11_211704) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.text "username", null: false
+    t.text "email", null: false
+    t.text "crypted_password", null: false
+    t.text "salt", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_authors_on_email", unique: true
   end
 
 end
